e32236f52839e16ae79f771ef80db4ba6719a16b
Marivatis <cyvax2@gmail.com>
2024-04-23 19:12:49 +0300

    Add customer profile form

---
+++ Delivery_Project/Forms/Customer/FormCustomerProfile.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Delivery_Project.Forms.Customer
+{
+    partial class FormCustomerProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelMyProfileTitle = new Label();
+            labelLogin = new Label();
+            textBoxLogin = new TextBox();
+            labelPhoneNumber = new Label();
+            textBoxPhoneNumber = new TextBox();
+            labelPassword = new Label();
+            textBoxPassword = new TextBox();
+            labelAddress = new Label();
+            buttonEdit1 = new Button();
+            labelBecome1 = new Label();
+            labelBecome2 = new Label();
+            labelBecomeProvider = new Label();
+            labelBecomeCourier = new Label();
+            buttonEdit2 = new Button();
+            textBoxAddress = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // labelMyProfileTitle
+            // 
+            labelMyProfileTitle.AutoSize = true;
+            labelMyProfileTitle.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            labelMyProfileTitle.ForeColor = Color.White;
+            labelMyProfileTitle.Location = new Point(90, 35);
+            labelMyProfileTitle.Name = "labelMyProfileTitle";
+            labelMyProfileTitle.Size = new Size(151, 37);
+            labelMyProfileTitle.TabIndex = 18;
+            labelMyProfileTitle.Text = "My Profile";
+            // 
+            // labelLogin
+            // 
+            labelLogin.AutoSize = true;
+            labelLogin.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelLogin.ForeColor = Color.White;
+            labelLogin.Location = new Point(40, 103);
+            labelLogin.Name = "labelLogin";
+            labelLogin.Size = new Size(75, 30);
+            labelLogin.TabIndex = 20;
+            labelLogin.Text = "Login:";
+            // 
+            // textBoxLogin
+            // 
+            textBoxLogin.BackColor = Color.FromArgb(61, 61, 61);
+            textBoxLogin.BorderStyle = BorderStyle.FixedSingle;
+            textBoxLogin.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            textBoxLogin.ForeColor = Color.White;
+            textBoxLogin.Location = new Point(40, 136);
+            textBoxLogin.Name = "textBoxLogin";
+            textBoxLogin.Size = new Size(250, 33);
+            textBoxLogin.TabIndex = 19;
+            textBoxLogin.Text = "Enter your login";
+            // 
+            // labelPhoneNumber
+            // 
+            labelPhoneNumber.AutoSize = true;
+            labelPhoneNumber.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelPhoneNumber.ForeColor = Color.White;
+            labelPhoneNumber.Location = new Point(40, 172);
+            labelPhoneNumber.Name = "labelPhoneNumber";
+            labelPhoneNumber.Size = new Size(166, 30);
+            labelPhoneNumber.TabIndex = 22;
+            labelPhoneNumber.Text = "Phone number:";
+            // 
+            // textBoxPhoneNumber
+            // 
+            textBoxPhoneNumber.BackColor = Color.FromArgb(61, 61, 61);
+            textBoxPhoneNumber.BorderStyle = BorderStyle.FixedSingle;
+            textBoxPhoneNumber.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            textBoxPhoneNumber.ForeColor = Color.White;
+            textBoxPhoneNumber.Location = new Point(40, 205);
+            textBoxPhoneNumber.Name = "textBoxPhoneNumber";
+            textBoxPhoneNumber.Size = new Size(250, 33);
+            textBoxPhoneNumber.TabIndex = 21;
+            textBoxPhoneNumber.Text = "Enter your phone number";
+            // 
+            // labelPassword
+            // 
+            labelPassword.AutoSize = true;
+            labelPassword.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelPassword.ForeColor = Color.White;
+            labelPassword.Location = new Point(40, 272);
+            labelPassword.Name = "labelPassword";
+            labelPassword.Size = new Size(111, 30);
+            labelPassword.TabIndex = 24;
+            labelPassword.Text = "Password:";
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.BackColor = Color.FromArgb(61, 61, 61);
+            textBoxPassword.BorderStyle = BorderStyle.FixedSingle;
+            textBoxPassword.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            textBoxPassword.ForeColor = Color.White;
+            textBoxPassword.Location = new Point(40, 305);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(250, 33);
+            textBoxPassword.TabIndex = 23;
+            textBoxPassword.Text = "Enter your password";
+            // 
+            // labelAddress
+            // 
+            labelAddress.AutoSize = true;
+            labelAddress.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelAddress.ForeColor = Color.White;
+            labelAddress.Location = new Point(40, 374);
+            labelAddress.Name = "labelAddress";
+            labelAddress.Size = new Size(97, 30);
+            labelAddress.TabIndex = 25;
+            labelAddress.Text = "Address:";
+            // 
+            // buttonEdit1
+            // 
+            buttonEdit1.FlatStyle = FlatStyle.Flat;
+            buttonEdit1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonEdit1.ForeColor = Color.White;
+            buttonEdit1.Location = new Point(224, 97);
+            buttonEdit1.Name = "buttonEdit1";
+            buttonEdit1.Size = new Size(66, 36);
+            buttonEdit1.TabIndex = 27;
+            buttonEdit1.Text = "Edit";
+            buttonEdit1.UseVisualStyleBackColor = true;
+            // 
+            // labelBecome1
+            // 
+            labelBecome1.AutoSize = true;
+            labelBecome1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            labelBecome1.ForeColor = Color.White;
+            labelBecome1.Location = new Point(40, 473);
+            labelBecome1.Name = "labelBecome1";
+            labelBecome1.Size = new Size(66, 21);
+            labelBecome1.TabIndex = 30;
+            labelBecome1.Text = "Become";
+            // 
+            // labelBecome2
+            // 
+            labelBecome2.AutoSize = true;
+            labelBecome2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            labelBecome2.ForeColor = Color.White;
+            labelBecome2.Location = new Point(175, 473);
+            labelBecome2.Name = "labelBecome2";
+            labelBecome2.Size = new Size(25, 21);
+            labelBecome2.TabIndex = 31;
+            labelBecome2.Text = "or";
+            // 
+            // labelBecomeProvider
+            // 
+            labelBecomeProvider.AutoSize = true;
+            labelBecomeProvider.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            labelBecomeProvider.ForeColor = Color.White;
+            labelBecomeProvider.Location = new Point(196, 473);
+            labelBecomeProvider.Name = "labelBecomeProvider";
+            labelBecomeProvider.Size = new Size(88, 21);
+            labelBecomeProvider.TabIndex = 32;
+            labelBecomeProvider.Text = "a provider";
+            // 
+            // labelBecomeCourier
+            // 
+            labelBecomeCourier.AutoSize = true;
+            labelBecomeCourier.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            labelBecomeCourier.ForeColor = Color.White;
+            labelBecomeCourier.Location = new Point(103, 473);
+            labelBecomeCourier.Name = "labelBecomeCourier";
+            labelBecomeCourier.Size = new Size(77, 21);
+            labelBecomeCourier.TabIndex = 33;
+            labelBecomeCourier.Text = "a courier";
+            // 
+            // buttonEdit2
+            // 
+            buttonEdit2.FlatStyle = FlatStyle.Flat;
+            buttonEdit2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonEdit2.ForeColor = Color.White;
+            buttonEdit2.Location = new Point(224, 263);
+            buttonEdit2.Name = "buttonEdit2";
+            buttonEdit2.Size = new Size(66, 36);
+            buttonEdit2.TabIndex = 34;
+            buttonEdit2.Text = "Edit";
+            buttonEdit2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAddress
+            // 
+            textBoxAddress.BackColor = Color.FromArgb(61, 61, 61);
+            textBoxAddress.BorderStyle = BorderStyle.FixedSingle;
+            textBoxAddress.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            textBoxAddress.ForeColor = Color.White;
+            textBoxAddress.Location = new Point(40, 407);
+            textBoxAddress.Name = "textBoxAddress";
+            textBoxAddress.Size = new Size(250, 33);
+            textBoxAddress.TabIndex = 35;
+            textBoxAddress.Text = "Enter your address";
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(224, 365);
+            button1.Name = "button1";
+            button1.Size = new Size(66, 36);
+            button1.TabIndex = 36;
+            button1.Text = "Edit";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // FormCustomerProfile
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(330, 520);
+            Controls.Add(button1);
+            Controls.Add(textBoxAddress);
+            Controls.Add(buttonEdit2);
+            Controls.Add(labelBecomeCourier);
+            Controls.Add(labelBecomeProvider);
+            Controls.Add(labelBecome2);
+            Controls.Add(labelBecome1);
+            Controls.Add(buttonEdit1);
+            Controls.Add(labelAddress);
+            Controls.Add(labelPassword);
+            Controls.Add(textBoxPassword);
+            Controls.Add(labelPhoneNumber);
+            Controls.Add(textBoxPhoneNumber);
+            Controls.Add(labelLogin);
+            Controls.Add(textBoxLogin);
+            Controls.Add(labelMyProfileTitle);
+            Name = "FormCustomerProfile";
+            Text = "FormCustomerProfile";
+            Controls.SetChildIndex(labelMyProfileTitle, 0);
+            Controls.SetChildIndex(textBoxLogin, 0);
+            Controls.SetChildIndex(labelLogin, 0);
+            Controls.SetChildIndex(textBoxPhoneNumber, 0);
+            Controls.SetChildIndex(labelPhoneNumber, 0);
+            Controls.SetChildIndex(textBoxPassword, 0);
+            Controls.SetChildIndex(labelPassword, 0);
+            Controls.SetChildIndex(labelAddress, 0);
+            Controls.SetChildIndex(buttonEdit1, 0);
+            Controls.SetChildIndex(labelBecome1, 0);
+            Controls.SetChildIndex(labelBecome2, 0);
+            Controls.SetChildIndex(labelBecomeProvider, 0);
+            Controls.SetChildIndex(labelBecomeCourier, 0);
+            Controls.SetChildIndex(buttonEdit2, 0);
+            Controls.SetChildIndex(textBoxAddress, 0);
+            Controls.SetChildIndex(button1, 0);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelMyProfileTitle;
+        private Label labelLogin;
+        private TextBox textBoxLogin;
+        private Label labelPhoneNumber;
+        private TextBox textBoxPhoneNumber;
+        private Label labelPassword;
+        private TextBox textBoxPassword;
+        private Label labelAddress;
+        private Button buttonEdit1;
+        private Label labelBecome1;
+        private Label labelBecome2;
+        private Label labelBecomeProvider;
+        private Label labelBecomeCourier;
+        private Button buttonEdit2;
+        private TextBox textBoxAddress;
+        private Button button1;
+    }
+}
